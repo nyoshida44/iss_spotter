c90537c209c7db3ad526be985a834151e7168171
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-05 13:58:11 +0000

    added promises method with iss_promised and index2.js

---
+++ index2.js
@@ -0,0 +1,11 @@
+const {nextISSTimesForMyLocation} = require("./iss_promised");
+
+nextISSTimesForMyLocation()
+  .then((passtimes) => {
+    for (const pass of passtimes) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
+  })
---
+++ iss_promised.js
@@ -0,0 +1,26 @@
+const request = require("request-promise-native");
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://api.freegeoip.app/json/${ip}?apikey=ff0b86e0-3dd3-11ec-b130-c5c1d00855da`)
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const {latitude, longitude} = JSON.parse(body)
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((body) => {
+      const { response } = JSON.parse(body);
+      return response;
+    });
+};
+module.exports = { nextISSTimesForMyLocation };
